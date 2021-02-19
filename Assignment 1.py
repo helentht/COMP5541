344736ad69f18442d4e02d28b55168ab17d1b800
Helen Tsui <helentsui.tht@gmail.com>
2021-02-19 15:13:01 +0800

    first commit

---
+++ Assignment 1.py
@@ -0,0 +1,12 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    nbc = NBC(feature types=[‘r’, ‘r’, ‘r’, ‘r’], num classes=3)
+    nbc.fit()
+    nbc.predict()
