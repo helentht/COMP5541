3605e61ff240451bcddf5a1e856a6591eb8595a9
Helen Tsui <helentsui.tht@gmail.com>
2021-02-19 15:25:20 +0800

    add class

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,27 +1,41 @@
 from sklearn.datasets import load_iris
+import numpy as np
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class NBC():
+    def __init__(self, feature_types, num_classes):
+        self.feature_types = feature_types
+        self.num_classes = num_classes
 
-def
+    def fit(self, Xtrain, ytrain):
 
+        return
+
+    def predict(self, Xtest):
+        return
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # load iris dataset
-    iris = load iris()
-    X, y = iris[‘data’], iris[‘target’]
+    iris = load_iris()
+    X, y = iris['data'], iris['target']
 
     # shuffle the dataset and put 20% aside for testing
     N, D = X.shape
     Ntrain = int(0.8 * N)
-    shuffler = np.random.permutation(N) Xtrain = X[shuffler[:Ntrain]]
+    shuffler = np.random.permutation(N)
+    Xtrain = X[shuffler[:Ntrain]]
     ytrain = y[shuffler[:Ntrain]]
     Xtest = X[shuffler[Ntrain:]]
     ytest = y[shuffler[Ntrain:]]
-    
-    nbc = NBC(feature types=[‘r’, ‘r’, ‘r’, ‘r’], num classes=3)
+
+    # initialize the Naive Bayes Classifier
+    nbc = NBC(feature_types=['r', 'r', 'r', 'r'], num_classes=3)
+
+    # fit the classifier to training data
     nbc.fit(Xtrain, ytrain)
+
+    # predict the classes
     yhat = nbc.predict(Xtest)
-    test accuracy = np.mean(yhat == ytest)
+
+    # evaluate the result
+    test_accuracy = np.mean(yhat == ytest)
