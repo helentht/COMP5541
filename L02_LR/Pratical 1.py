7fb3bda3f1a558249e0b40c4552eac89c7043ee8
Helen Tsui <helentsui.tht@gmail.com>
2021-02-23 22:39:10 +0800

    add new folder L02_LR

---
+++ L02_LR/Pratical 1.py
@@ -0,0 +1,23 @@
+import numpy as np # linear algebra
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def load_data():
+
+    ## x:   Nx1 (N datapoints with 1 feature)
+    ## y:   Nx1
+    return x_train, y_train, x_test, y_test
+
+def gradient_descent(x_train, y_train):
+
+    return w1, w2, b
+
+def evaluation(w1, w2, x_test, y_test):
+
+    return err
+
+
+
+if __name__ == '__main__':
+    demo()
